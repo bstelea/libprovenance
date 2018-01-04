bd15e4f37a4a1ab3d3f75f2c053ef47f15af5211
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-01-04 09:59:56 +0000

    update copyright statement

--- src/libprovenance.c
+++ src/libprovenance.c
@@ -2,7 +2,7 @@
 *
 * Author: Thomas Pasquier <tfjmp2@cam.ac.uk>
 *
-* Copyright (C) 2015 University of Cambridge
+* Copyright (C) 2015-2018 University of Cambridge, Harvard University
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License version 2, as
