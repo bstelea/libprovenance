c04c3e13cc78dbe6a2d1ac1d99932f82317dd3f1
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-03-22 15:59:13 +0000

    more basic structure in provenanceSPADEJSON.c

--- src/provenanceSPADEJSON.c
+++ src/provenanceSPADEJSON.c
@@ -35,56 +35,61 @@
 #include "provenanceSPADEJSON.h"
 #include "provenanceutils.h"
 
-char* relation_to_spade_json(struct relation_struct* e) {
+#define MAX_SPADEJSON_BUFFER_EXP     13
+#define MAX_SPADEJSON_BUFFER_LENGTH  ((1 << MAX_SPADEJSON_BUFFER_EXP)*sizeof(uint8_t))
 
+static __thread char buffer[MAX_SPADEJSON_BUFFER_LENGTH];
+
+char* relation_to_spade_json(struct relation_struct* e) {
+  return buffer;
 }
 
 char* used_to_spade_json(struct relation_struct* e) {
-
+  return buffer;
 }
 
 char* generated_to_spade_json(struct relation_struct* e) {
-
+  return buffer;
 }
 
 char* informed_to_spade_json(struct relation_struct* e) {
-
+  return buffer;
 }
 
 char* derived_to_spade_json(struct relation_struct* e) {
-
+  return buffer;
 }
 
 char* disc_to_spade_json(struct disc_node_struct* n) {
-
+  return buffer;
 }
 
 char* proc_to_spade_json(struct proc_prov_struct* n) {
-
+  return buffer;
 }
 
 char* task_to_spade_json(struct task_prov_struct* n) {
-
+  return buffer;
 }
 
 char* inode_to_spade_json(struct inode_prov_struct* n) {
-
+  return buffer;
 }
 
 char* sb_to_spade_json(struct sb_struct* n) {
-
+  return buffer;
 }
 
 char* msg_to_spade_json(struct msg_msg_struct* n) {
-
+  return buffer;
 }
 
 char* shm_to_spade_json(struct shm_struct* n) {
-
+  return buffer;
 }
 
 char* packet_to_spade_json(struct pck_struct* n) {
-
+  return buffer;
 }
 
 char* str_msg_to_spade_json(struct str_struct* n) {
@@ -92,25 +97,25 @@ char* str_msg_to_spade_json(struct str_struct* n) {
 }
 
 char* addr_to_spade_json(struct address_struct* n) {
-
+  return buffer;
 }
 
 char* pathname_to_spade_json(struct file_name_struct* n) {
-
+  return buffer;
 }
 
 char* iattr_to_spade_json(struct iattr_prov_struct* n) {
-
+  return buffer;
 }
 
 char* xattr_to_spade_json(struct xattr_prov_struct* n) {
-
+  return buffer;
 }
 
 char* pckcnt_to_spade_json(struct pckcnt_struct* n) {
-
+  return buffer;
 }
 
 char* arg_to_spade_json(struct arg_struct* n) {
-
+  return buffer;
 }
